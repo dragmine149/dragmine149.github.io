7307e002bf9a4dbc48592bf994099b3566b29b47
dragmine <dan@thebanners.uk>
2025-01-31 21:22:32 +0000

    Blog of 31st Janurary

--- Blog/blog.js
+++ Blog/blog.js
@@ -80,30 +80,6 @@ function markedLocalTime() {
     }],
   }
 }
-/*
-const description = {
-  name: 'description',
-  level: 'inline',                                 // Is this a block-level or inline-level tokenizer?
-  start(src) { return src.match(/:/)?.index; },    // Hint to Marked.js to stop and check for a match
-  tokenizer(src, tokens) {
-    const rule = /^:([^:\n]+):([^:\n]*)(?:\n|$)/;  // Regex for the complete token, anchor to string start
-    const match = rule.exec(src);
-    if (match) {
-      return {                                         // Token to generate
-        type: 'description',                           // Should match "name" above
-        raw: match[0],                                 // Text to consume from the source
-        dt: this.lexer.inlineTokens(match[1].trim()),  // Additional custom properties, including
-        dd: this.lexer.inlineTokens(match[2].trim())   //   any further-nested inline tokens
-      };
-    }
-  },
-  renderer(token) {
-    return `\n < dt > ${ this.parser.parseInline(token.dt) }</dt > <dd>${this.parser.parseInline(token.dd)}</dd>`;
-  },
-  childTokens: ['dt', 'dd'],                 // Any child tokens to be visited by walkTokens
-};
-
-*/
 
 class Blog {
   constructor() {
