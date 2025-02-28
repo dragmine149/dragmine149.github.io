b9e7969ebf88394b8b9d57f04b4c890f6edcf200
dragmine <dan@thebanners.uk>
2025-02-28 23:23:03 +0000

    Used new system for getting settings updates, prevents issues when settings are still being loaded

--- Scripts/Particles/particle_controller.js
+++ Scripts/Particles/particle_controller.js
@@ -9,25 +9,6 @@ function disable_particles() {
   document.getElementById('particles-js').childNodes.forEach((child) => child.remove());
 }
 
-// load_particles();
-
-/** @param {HTMLInputElement} obj */
-function snow_toggle(obj) {
-  switch (obj.checked) {
-    case true:
-      load_particles();
-      // settings.setStorage("particles", "1");
-      break;
-    case false:
-      disable_particles();
-      // settings.setStorage("particles", "0");
-      break;
-    default:
-      console.error("Somehow a checkbox is not checked or not unchecked?");
-      break;
-  }
-}
-
-const snow_settings_elm = document.getElementById("Settings-snow");
-snow_settings_elm.checked = settings.get_setting("Particle", "Enabled") ?? true;
-snow_toggle(snow_settings_elm);
+settings.add_listener("Particle", "Enabled", (v) => {
+  v ? load_particles() : disable_particles();
+});
