244f2d3afd8a36c85ed0e1e0ad33b3671b85e669
Dragmine149 <dan@thebanners.uk>
2025-08-10 00:33:33 +0100

    Clean up repo a tad

--- bun_build.sh
+++ bun_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bun build --watch --outdir=./dist --minify --sourcemap=linked --splitting ./Scripts/initial.ts ./Scripts/code_copier.ts ./Scripts/debug.ts
-# bun build --watch --outdir=./external ./Scripts/storage.ts
+bun build --outdir=./dist --minify --sourcemap=linked --splitting ./Scripts/initial.ts ./Scripts/code_copier.ts ./Scripts/debug.ts
+bun build --outdir=./external ./Scripts/storage.ts
